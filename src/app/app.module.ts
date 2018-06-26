086b65eed05320b4c115d64c84a28e6105aeb4d8
Andrew Hughson <andrew@hughson.me>
2018-06-26 23:29:20 +1200

    Remove unused routing file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,19 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PhotoCardComponent } from './photo-card/photo-card.component';
 import { PagingControlComponent } from './paging-control/paging-control.component';
+import { PhotoCardComponent } from './photo-card/photo-card.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 
 @NgModule({
-  declarations: [AppComponent, PhotoCardComponent, PagingControlComponent, PhotoListComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    AppRoutingModule
+  declarations: [
+    AppComponent,
+    PhotoCardComponent,
+    PagingControlComponent,
+    PhotoListComponent
   ],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
