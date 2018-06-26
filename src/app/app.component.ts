425f79b66d92a0e65e8b6f1cde61ae2d7acb1e9b
Andrew Hughson <andrew@hughson.me>
2018-06-26 22:29:35 +1200

    Fix photos not showing until something is searched

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,21 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { combineLatest, Observable, timer } from 'rxjs';
-import { debounce, filter, map } from 'rxjs/operators';
+import { debounce, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   searchField = new FormControl();
+  isLoading = true;
 
-  request$ = this.http.get<Photo[]>(
-    'http://jsonplaceholder.typicode.com/photos'
-  );
+  request$ = this.http
+    .get<Photo[]>('http://jsonplaceholder.typicode.com/photos')
+    .pipe(tap(() => (this.isLoading = false)));
 
   photos$: Observable<Photo[]> = combineLatest(
     this.searchField.valueChanges,
@@ -22,11 +23,17 @@ export class AppComponent {
   ).pipe(
     debounce(() => timer(200)),
     map(([query, photos]: [string, Photo[]]) => {
-      return query ? photos.filter(p => p.title.includes(query)) : photos;
+      return query
+        ? photos.filter(p => p.title.includes(query.toLowerCase()))
+        : photos;
     })
   );
 
   constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    setTimeout(() => this.searchField.setValue(''));
+  }
 }
 
 export interface Photo {
