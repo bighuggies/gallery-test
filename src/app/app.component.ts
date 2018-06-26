87b757e05dd1472ed7bc03918955690ac1feaee1
Andrew Hughson <andrew@hughson.me>
2018-06-26 21:51:41 +1200

    Add basic code for fetching photos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<Photo[]>('http://jsonplaceholder.typicode.com/photos')
+      .subscribe(p => console.log(p));
+  }
+}
+
+export interface Photo {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
 }
