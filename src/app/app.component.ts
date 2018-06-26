533f326a60d34cc5129328211925630477c8de58
Andrew Hughson <andrew@hughson.me>
2018-06-26 23:01:29 +1200

    Add paging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { combineLatest, Observable, timer } from 'rxjs';
+import { combineLatest, fromEvent, Observable, Subject, timer } from 'rxjs';
 import { debounce, map, tap } from 'rxjs/operators';
 
 @Component({
@@ -11,7 +11,11 @@ import { debounce, map, tap } from 'rxjs/operators';
 })
 export class AppComponent implements OnInit {
   searchField = new FormControl();
+  currentPage$ = new Subject<number>();
+
   isLoading = true;
+  pageSize = 50;
+  pageNumber = 1;
 
   request$ = this.http
     .get<Photo[]>('http://jsonplaceholder.typicode.com/photos')
@@ -19,24 +23,41 @@ export class AppComponent implements OnInit {
 
   photos$: Observable<Photo[]> = combineLatest(
     this.searchField.valueChanges,
+    this.currentPage$,
     this.request$
   ).pipe(
     debounce(() => timer(200)),
-    map(([query, photos]: [string, Photo[]]) => {
-      return query
+    map(([query, currentPage, photos]: [string, number, Photo[]]) => {
+      return (query
         ? photos.filter(p => p.title.includes(query.toLowerCase()))
-        : photos;
+        : photos
+      ).slice((currentPage - 1) * this.pageSize, currentPage * this.pageSize);
     })
   );
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    setTimeout(() => this.searchField.setValue(''));
+    setTimeout(() => {
+      this.searchField.setValue('');
+      this.currentPage$.next(1);
+    });
   }
 
   trackByFn(i: number, photo: Photo) {
     return photo.id;
+  }
+
+  nextPage() {
+    this.pageNumber = this.pageNumber + 1;
+
+    this.currentPage$.next(this.pageNumber);
+  }
+
+  prevPage() {
+    this.pageNumber = Math.max(1, this.pageNumber - 1);
+
+    this.currentPage$.next(this.pageNumber);
   }
 }
 
